f85ada06bcafce9f9c9faf9dfaf469ee7921d7be
MikeRowManage <michael.priestley88@yahoo.com>
2020-02-18 18:04:14 -0700

    Bcrypt afternoon completed

---
+++ server/index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express')
+const session = require('express-session')
+const massive = require('massive')
+const {SESSION_SECRET, CONNECTION_STRING} = process.env
+const authCtrl = require('./controllers/authController')
+const treasureCtrl = require('./controllers/treasureController')
+const auth = require('./middleware/authMiddleware')
+const app = express()
+
+const PORT = 4000
+
+app.use(express.json())
+app.use(
+    session({
+        resave: true,
+        saveUninitialized: false,
+        secret: SESSION_SECRET
+    })
+)
+
+app.post('/auth/register', authCtrl.register)
+app.post('/auth/login', authCtrl.login)
+app.get('/auth/logout', authCtrl.logout)
+app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure)
+app.get('/api/treasure/user', auth.usersOnly, treasureCtrl.getUserTreasure)
+app.post('/api/treasure/user', auth.usersOnly, treasureCtrl.addUserTreasure)
+app.get('/api/treasure/all', auth.usersOnly, auth.adminsOnly, treasureCtrl.getAllTreasure)
+// app.get('/api/treasure/all', auth.adminsOnly, treasureCtrl.getAllTreasure)
+
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db)
+})
+
+app.listen(PORT, () => console.log(`Server is a go for Port: ${PORT}!`))
--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -19,15 +19,36 @@ export default class Container extends Component {
   }
 
   getDragonTreasure() {
-    // axios GET to /api/treasure/dragon here
+    axios.get('/api/treasure/dragon').then(response => {
+      this.setState({
+        treasures: {
+          ...this.state.treasures,
+          dragon: response.data
+        }
+      })
+    }).catch(err => console.log(err))
   }
 
   getAllTreasure() {
-    // axios GET to /api/treasure/all here
+    axios.get('/api/treasure/all').then(res => {
+      this.setState({
+        treasures: {
+          ...this.state.treasures,
+          all: res.data
+        }
+      })
+    }).catch(err => alert(err.response.request.response))
   }
 
   getMyTreasure() {
-    // axios GET to /api/treasure/user here
+    axios.get('/api/treasure/user').then(response => {
+      this.setState({
+        treasures: {
+          ...this.state.treasures,
+          user: response.data
+        }
+      })
+    }).catch(err => alert(err.response.request.response))
   }
 
   addMyTreasure(newMyTreasure) {
